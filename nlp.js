5e8c75a05eae167e3dc027f51e4ace51a91adc9e
Lucas Della Bella <Lucas Della Bella>
2016-04-09 17:29:53 -0400

    Correct nlp.js. Two options, 1 callback

--- nlp.js
+++ nlp.js
@@ -1,17 +1,31 @@
 var request = require('request');
 
 module.exports = {
-	relationOptions: {
+    relationOptions: {
+            form: {
+              text: 'let\'s go to Emory University this Tuesday at 6 pm ',
+              apikey: '42517e512003fd0f9adeaa00bf892c58e5e8eb09',
+              outputMode: 'json',
+              //entities: '1',
+              showSourceText: 0,
+            },
+            method: 'POST',
+            url: 'https://gateway-a.watsonplatform.net/calls/text/TextGetRelations',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        },
+
+    dateTimeOptions: {
             form: {
                apikey: '42517e512003fd0f9adeaa00bf892c58e5e8eb09',
                outputMode: 'json',
                showSourceText: 0,
                anchorDate: '2016-04-09 11:11:11',
                text: 'let\'s go to school this Tuesday at 6 pm ',
-	       entities: '1'
             },
             method: 'POST',
-            url: 'https://gateway-a.watsonplatform.net/calls/text/TextGetRelations',
+            url: 'https://gateway-a.watsonplatform.net/calls/text/TextExtractDates',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
             }
