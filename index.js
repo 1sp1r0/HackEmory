42851d14015e9791248817a3386c262948355f69
joewang704 <joewang704@gmail.com>
2016-04-09 11:24:11 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var portNum = process.env.PORT || 8888
+
+app.get('/', function(req, res) {
+  return res.send("Hello World");
+})
+
+app.listen(portNum, () => {
+  if (!process.env.PORT) {
+    console.log(`Serving port number ${portNum}`)
+  }
+})
