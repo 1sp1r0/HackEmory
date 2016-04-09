aa85c39a2fe0553af4bafebcdbb128935f0f8529
Mete0 <majitsu@gmail.com>
2016-04-09 13:32:35 -0400

    slackhook again

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
 var app = express();
-
+var Slackhook = require('slackhook');
+var slack = new Slackhook({
+    domain: 'yourdomain',
+    token: 'yourtoken'
+});
 var portNum = process.env.PORT || 8888;
 var slackWebhookUrl = 'https://hooks.slack.com/services/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';
 
-app.use(bodyParser);
 
 app.post('/', function(req, res){
     var hook = slack.respond(req.body);
