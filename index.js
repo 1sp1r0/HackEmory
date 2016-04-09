9756f08dfe48e260bd7aa0ae9c5fb37b7b96d305
joewang704 <joewang704@gmail.com>
2016-04-09 12:22:12 -0400

    Working outgoing messages and incoming messages

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
 var app = express();
 
 var portNum = process.env.PORT || 8888;
-var bodyParser = require('body-parser');
+var slackWebhookUrl = 'https://hooks.slack.com/services/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';
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-
+app.use(bodyParser.json());
 
 app.post('/', function(req, res) {
   console.log(req.body);
   return res.send("Hello World");
+}
+
+app.get('/sendMsg', function(req, res) {
+  sendMessage("hello");
+  res.send("Hello World");
 })
+
+function sendMessage(msg) {
+  request({
+    method: 'POST',
+    uri: slackWebhookUrl,
+    json: {
+      text: msg
+    }
+  }, function(error, response, body) {
+    console.log(response);
+  });
+}
+
+
 
 app.listen(portNum, () => {
   if (!process.env.PORT) {
-    console.log("Serving port number " + portNum)
+    console.log("Serving port number " + portNum);
   }
 })
 
